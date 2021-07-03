86c39aaff753c1543ec2fccd1084e32607d259c1
MostafaEhab <mostafa.ehab.wahdan@gmail.com>
2021-07-04 00:20:51 +0200

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import "./topbar.scss";
+import { Mail, LinkedIn } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            datasavvy.
+          </a>
+          <div className="itemContainer">
+            <LinkedIn className="icon" />
+            <span>
+              <a href="https://www.linkedin.com/in/mostafawahdan/">LinkedIn</a>
+            </span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>mostafa@datasavvy.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
