d113f93a66140423bd0da7fcaf9d332e9ec63ee6
MostafaEhab <mostafa.ehab.wahdan@gmail.com>
2021-07-05 10:19:17 +0200

    Edited works

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,36 +1,41 @@
 import { useState } from "react";
 import "./works.scss";
-import abtest from "../../img/ab-test.png"
 
 export default function Works() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const data = [
     {
       id: "1",
-      icon: "./assets/ab-testing-app.png",
-      title: "Analyze-A-B-Tests",
+      icon: "./assets/prosper-explore.webp",
+      title: "Data Analysis",
       desc:
-        "For this project, I interpreted the results of an A/B test run by an e-commerce website to decide whether they should change the website design.",
+        "In this project, I explored Prosper loans' dataset and gained interesting insights. This dataset contains 113,937 loans with 81 variables on each loan.",
       img:
-        {abtest},
+        "./assets/explore.png",
+      link:
+        "https://github.com/MostafaWahdan/Prosper-Loan-Project"
     },
     {
       id: "2",
-      icon: "./assets/globe.png",
-      title: "Mobile Application",
+      icon: "./assets/ab-testing-app.png",
+      title: "Analyze A/B Test",
       desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "For this project, I interpreted the results of an A/B test run by an e-commerce website to decide whether they should change the website design.",
       img:
-        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+        "./assets/ab-test.png",
+      link:
+        "https://github.com/MostafaWahdan/Analyze-A-B-Tests"
     },
     {
       id: "3",
-      icon: "./assets/writing.png",
-      title: "Branding",
+      icon: "./assets/scrape-icon.png",
+      title: "Web Scraping",
       desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "In this project, I created a web scraper to get movies' data from YTS website. This can be used to automatically track new releases.",
       img:
-        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+        "./assets/web-scraping.png",
+      link:
+        "https://github.com/MostafaWahdan/YTS_Scraper"
     },
   ];
 
@@ -56,12 +61,12 @@ export default function Works() {
                   </div>
                   <h2>{d.title}</h2>
                   <p>{d.desc}</p>
-                  <span><a href="https://github.com/MostafaWahdan/Analyze-A-B-Tests">Projects</a></span>
+                  <a href={d.link}>Source Code</a>
                 </div>
               </div>
               <div className="right">
                 <img
-                  src={abtest}
+                  src={d.img}
                   alt=""
                 />
               </div>
