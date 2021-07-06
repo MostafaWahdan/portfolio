e7d50e1912cb33215ad83814b56bfce44f038be8
MostafaEhab <mostafa.ehab.wahdan@gmail.com>
2021-07-06 06:49:18 +0200

    Updated contact form and portfolio title

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,7 +15,7 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form action="https://formsubmit.co/mostafa@datatalks.com" method="POST" onSubmit={handleSubmit}>
+        <form action="https://formsubmit.co/mostafa@datatalks.me" method="POST" onSubmit={handleSubmit}>
           <input type="hidden" name="_subject" value="Portfolio New submission!" />
           <input type="text" placeholder="Email" name="email" required />
           <textarea placeholder="Message" name="message" required></textarea>
