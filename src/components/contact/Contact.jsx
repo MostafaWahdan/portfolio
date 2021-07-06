f72a5e2d75c3ad37dd24d903f7658624448b87ad
MostafaEhab <mostafa.ehab.wahdan@gmail.com>
2021-07-06 07:49:32 +0200

    Updated contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,7 +15,8 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
-        <form action="https://formsubmit.co/mostafa@datatalks.me" method="POST" onSubmit={handleSubmit}>
+        <form action="https://formsubmit.co/mostafa.ehab.wahdan@gmail.com" method="POST">
+        {/* onSubmit={handleSubmit} */}
           <input type="hidden" name="_subject" value="Portfolio New submission!" />
           <input type="text" placeholder="Email" name="email" required />
           <textarea placeholder="Message" name="message" required></textarea>
